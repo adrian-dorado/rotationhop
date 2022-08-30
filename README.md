bcb9af78f2e0be6cd1031f9996086e4704610c6a
rbbitphoto <rbbitphoto@gmail.com>
2022-08-30 01:50:45 -0700

    created app

--- README.md
+++
@@ -1,2 +0,0 @@
-# rotationhop
-ffxiv rotation tracker built with django and react + tailwind
---
+++ ffxiv_django/rotation_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RotationAppConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "rotation_app"
